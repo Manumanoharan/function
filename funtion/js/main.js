f03988db260021f934869c2ea0662452f6116bd5
Manu <90511900+Manumanoharan@users.noreply.github.com>
2022-08-30 14:17:13 +0530

    Add files via upload

---
+++ funtion/js/main.js
@@ -0,0 +1,20 @@
+var a=23;
+var b=24
+
+
+function name(){
+    if(a<b){
+      var c=a;
+        console.log(a);
+       return c; 
+    }
+    else{
+        var d=b;
+        console.log(b)
+        return d;
+    }
+
+}
+
+var name1= name();
+console.log(name1);
